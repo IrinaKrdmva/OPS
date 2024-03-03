92c702140b6db933a962a7b3fab906d538563b3c
Irina Karadimova <irinamaple84@gmail.com>
2024-03-03 23:09:56 +0000

    update nav

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 "use strict";
-function classToggle() {
-  const navs = document.querySelectorAll(".Navbar__Items");
-  navs.forEach((nav) => nav.classList.toggle("Navbar__ToggleShow"));
-}
-document
-  .querySelector(".Navbar__Link-toggle")
-  .addEventListener("click", classToggle);

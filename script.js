27fe5eb259d7da203e216a9526546c04b79f6999
Irina Karadimova <irinamaple84@gmail.com>
2024-03-05 18:03:35 +0000

    Update Nav

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+const hamburguer = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLogo = document.querySelector(".nav-logo");
+
+hamburguer.addEventListener("click", function () {
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  navLogo.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", function () {
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active");
+    navLogo.classList.remove("active");
+  })
+);

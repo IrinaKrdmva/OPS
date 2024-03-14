0377c486b3d6c047e7b470b0c16a933a6bbfe08c
Irina Karadimova <irinamaple84@gmail.com>
2024-03-14 21:37:34 +0000

    Update Footer

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict";
+
+//----SIDEBAR----
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
@@ -9,11 +11,13 @@ function hideSidebar() {
   sidebar.style.display = "none";
 }
 
+//----FEEDBACK SWIPER----
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
+  initialSlide: 2,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
@@ -25,3 +29,26 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+//----BRANDS SCROLLER----
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller-inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
